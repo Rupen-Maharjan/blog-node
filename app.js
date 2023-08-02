2b0976195e173a61940f7f8851de1f14f73ce331
Discort <112250385+Rupen-Maharjan@users.noreply.github.com>
2023-08-02 15:23:42 +0545

    Update app.js

--- app.js
+++ app.js
@@ -92,8 +92,9 @@ app.post("/compose",(req,res)=>{
   })
 })
 
+const port = process.env.PORT || 5000 
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started on port "+port);
 });
+
